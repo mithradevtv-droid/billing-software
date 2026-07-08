6bfca3b74c21e5cf766ff3f4ab56874893733091
Mithradev T V <mithradevtv@gmail.com>
2026-07-08 20:00:29 +0530

    Fix import case for Header component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,6 @@
 import { Sidebar } from '@/components/layout/sidebar'
 import { MobileNav } from '@/components/layout/mobile-nav'
-import { Header } from '@/components/layout/header'
+import { Header } from '@/components/layout/Header'
 
 export default function DashboardLayout({
   children,
